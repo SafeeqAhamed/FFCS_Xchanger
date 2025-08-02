714bcfd19a925161bff98d99dfbe0c6a58b3b15f
Safeeq Ahamed S <ssafeeq2004@gmail.com>
2025-08-02 23:16:36 +0530

    Fix: removed duplicate path require

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,7 @@ app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-
-const path = require("path");
+ 
 
 // Serve frontend **only in production**
 if (process.env.NODE_ENV === "production") {
